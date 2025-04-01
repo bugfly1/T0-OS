bed38a78392d0559781b2460190f768eb9e981d8
Rodrigo Flores <rodrigo.flores@uc.cl>
2025-04-01 19:42:19 -0300

    feat: quit

--- src/dccAdmin/main.c
+++ src/dccAdmin/main.c
@@ -9,6 +9,9 @@ int procesos_activos;
 bool loop = true;
 bool closeShell = false;
 
+// Variable global para modificar el quit
+bool quit = false;
+
 
 void sigchld_handler(int signal){
   printf("Ejecutando sigchld handler\n");
@@ -17,6 +20,18 @@ void sigchld_handler(int signal){
     pid_t pid = waitpid(-1, &status, WNOHANG);
     if (pid <= 0){
       break;
+    }
+    if (WEXITSTATUS(status) == 22){
+      printf("Eliminando procesos después de los 10 segundos\n");
+      ch_p* temp = child;
+      while(temp != NULL){
+        if (temp->exit_code == -1){
+          printf("Proceso %d\n", temp->pid);
+          kill(temp->pid, SIGKILL);
+        }
+        temp = temp->next;
+      }
+      quit = true;
     }
     if (WIFEXITED(status) || WIFSIGNALED(status)){
       printf("Se terminó el proceso %d con term signal %d\n", pid, WTERMSIG(status));
@@ -55,13 +70,23 @@ void sigctrlC_handler(int sig){
 
 void handle_alarm(int sig){
   ch_p* temp = child;
+  time_t tiempo_f;
   if (procesos_activos != 0){
     printf("Timeout cumplido!\n");
     while (temp != NULL){
       if (temp->exit_code == -1){
-        printf("Terminando el procesos %d\n", temp->pid);
+        printf("Terminando el proceso %d\n", temp->pid);
         kill(temp->pid, SIGTERM);
-      }
+        if (temp->exit_code == -1){
+          tiempo_f = time(NULL);
+          }
+          else{
+          tiempo_f = temp->tiempo_final;
+          }
+          printf("|  PID  | Executable | Exec Time | Exit code | Signal Value |\n");
+        printf("| %-5d | %-10s |    %-6ld |    %-6d |      %-7d |\n", 
+          temp->pid, temp->exec_name, tiempo_f - temp->tiempo_inicial, temp->exit_code, temp->signal_value);
+    }
       temp = temp->next;
     }
   }
@@ -87,6 +112,7 @@ int main(int argc, char const *argv[])
     time_max = atoi(argv[1]);
   }
 
+
   
   while (loop)
   {
@@ -111,7 +137,7 @@ int main(int argc, char const *argv[])
             // Significa que aún no se le ha enviado una señal para que termine
             printf("El proceso %i lleva %li segundos!!\n", temp->pid, tiempo_proceso);
             printf("Usando SIGTERM en el proceso %d\n", temp->pid);
-            kill_ok = kill(SIGTERM, temp->pid);
+            kill_ok = kill(temp->pid, SIGTERM);
             if (kill_ok == 0){
               printf("Funcionó\n");
             }
@@ -196,23 +222,6 @@ int main(int argc, char const *argv[])
         printf("Vamos a esperar %d segundos\n", time);
         alarm(time);
       }
-      // float time = atof(time_string);
-      // clock_t time_i = clock();
-      // clock_t tiempo_transcurrido;
-      // float tiempo_seg = 0;
-      // // Validar si existen procesos en ejecucion
-      // if (procesos_activos == 0){
-      //   printf("No existen procesos en ejecución. Timeout no se puede ejecutar\n");
-      // }
-      // else{
-      //   printf("Vamos a esperar %f segundos\n", time);
-      //   while(tiempo_seg < time){
-      //     tiempo_transcurrido = clock() - time_i;
-      //     tiempo_seg = (float) tiempo_transcurrido/CLOCKS_PER_SEC;
-      //   }
-      //   printf("Tiempo cumplido! (pasaron %f segundos)\n", tiempo_seg);
-      //   sigterm_childs(child);
-      // }
 
     }
       
@@ -221,12 +230,38 @@ int main(int argc, char const *argv[])
       printf("bye bye\n");
       
       // Enviar SIGINT
+      ch_p* temp = child;
+      if (procesos_activos != 0){
+        while (temp != NULL){
+          if (temp->exit_code != -1){
+            kill(temp->pid, SIGINT);
+            temp->senal_enviada = true;
+          }
+          temp = temp->next;
+        }
+      }
+      pid = fork();
+      if (pid == 0){
+        // Proceso hijo para revisar tiempo
+        printf("Esperando 10 segundos...\n");
+        clock_t start_time = clock();
+        clock_t tiempo_transcurrido;
+        float tiempo_seg = 0;
+        while (tiempo_seg < 10){
+            tiempo_transcurrido = clock() - start_time;
+            tiempo_seg = tiempo_transcurrido/CLOCKS_PER_SEC;
+        }        
+        printf("Pasaron 10 segundos!\n");
+        return 22;
+      }
+      else{
+      }
+    }
+    if (quit){
       destroy_child(child);
       free_user_input(input);
       break;
     }
-
-    free_user_input(input);
   }
   
 
